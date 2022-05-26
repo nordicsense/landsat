864d97e695d86e71ed8e7b5402b897d340a1b5ef
Oleg Sklyar <osklyar@sklyar.io>
2022-05-26 14:27:07 +0200

    Simplifies the run-through shell script

--- convert.sh
+++ convert.sh
@@ -1,41 +1,32 @@
 #!/usr/bin/env sh
 
-mkdir -p /Volumes/Caffeine/Data/Landsat/corrected/training
-landsat correct -v -s \
-  -d /Volumes/Caffeine/Data/Landsat/sources/training \
-  -o /Volumes/Caffeine/Data/Landsat/corrected/training # compress=deflate zlevel=6 predictor=3
+export ROOT_DIR=/Volumes/Caffeine/Data/Landsat
+mkdir -p ${ROOT_DIR}/corrected/training
+mkdir -p ${ROOT_DIR}/corrected/prod
 
-mkdir -p /Volumes/Caffeine/Data/Landsat/corrected/prod
-landsat correct -v -s \
-  -d /Volumes/Caffeine/Data/Landsat/sources/prod \
-  -o /Volumes/Caffeine/Data/Landsat/corrected/prod # compress=deflate zlevel=6 predictor=3
+landsat correct -v -s -d ${ROOT_DIR}/sources/training -o ${ROOT_DIR}/corrected/training # compress=deflate zlevel=6 predictor=3
 
-mkdir -p /Volumes/Caffeine/Data/Landsat/results/current/trainingdata
-landsat field /Volumes/Caffeine/Data/Landsat/sources/training-coordinates \
-  -d /Volumes/Caffeine/Data/Landsat/corrected/training \
-  -o /Volumes/Caffeine/Data/Landsat/results/current/trainingdata/trainingdata
+landsat correct -v -s -d ${ROOT_DIR}/sources/prod -o ${ROOT_DIR}/corrected/prod # compress=deflate zlevel=6 predictor=3
 
+export RESULTS_DIR=${ROOT_DIR}/results/current
+mkdir -p ${RESULTS_DIR}/trainingdata
 
-mkdir -p /Volumes/Caffeine/Data/Landsat/results/current
+landsat field ${ROOT_DIR}/sources/training-coordinates -d ${ROOT_DIR}/corrected/training -o ${RESULTS_DIR}/trainingdata/trainingdata
 
-for TIFFNAME in /Volumes/Caffeine/Data/Landsat/corrected/prod/LC08*.tiff; do
+# Train the model here
+
+for TIFFNAME in ${ROOT_DIR}/corrected/prod/LC08*.tiff; do
   echo $TIFFNAME
-  landsat predict -v --id=8 "$TIFFNAME" \
-    -m /Volumes/Caffeine/Data/Landsat/results/current/tf.model \
-    -o /Volumes/Caffeine/Data/Landsat/results/current
+  landsat predict -v --id=8 "$TIFFNAME" -m ${RESULTS_DIR}/tf.model -o ${RESULTS_DIR}
 done
 
-for TIFFNAME in /Volumes/Caffeine/Data/Landsat/corrected/prod/LE07*.tiff; do
+for TIFFNAME in ${ROOT_DIR}/corrected/prod/LE07*.tiff; do
   echo $TIFFNAME
-  landsat predict -v --id=7 "$TIFFNAME" \
-    -m /Volumes/Caffeine/Data/Landsat/results/current/tf.model \
-    -o /Volumes/Caffeine/Data/Landsat/results/current
+  landsat predict -v --id=7 "$TIFFNAME" -m ${RESULTS_DIR}/tf.model -o ${RESULTS_DIR}
 done
 
-for TIFFNAME in /Volumes/Caffeine/Data/Landsat/corrected/prod/LT05*.tiff; do
+for TIFFNAME in ${ROOT_DIR}/corrected/prod/LT05*.tiff; do
   echo $TIFFNAME
-  landsat predict -v --id=5 "$TIFFNAME" \
-    -m /Volumes/Caffeine/Data/Landsat/results/current/tf.model \
-    -o /Volumes/Caffeine/Data/Landsat/results/current
+  landsat predict -v --id=5 "$TIFFNAME" -m ${RESULTS_DIR}/tf.model -o ${RESULTS_DIR}
 done
 
